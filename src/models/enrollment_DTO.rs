a631b941d5de6bb76dd3fd22ee1238bd4794b74f
ayman2ppp2 <79267200+ayman2ppp2@users.noreply.github.com>
2026-01-03 23:38:46 +0200

    Update enrollment_DTO.rs changed the paring from der to pem

--- src/models/enrollment_DTO.rs
+++ src/models/enrollment_DTO.rs
@@ -18,9 +18,9 @@ pub struct IntermediateEnrollDto{
 impl EnrollDTO{
   pub async fn parse(&self) -> Result<IntermediateEnrollDto,String>{
     let certificate_bytes = general_purpose::STANDARD.decode(& self.csr_base64).map_err(|_|
-      "the certificate is not valid base64"
+      "the certificate request is not valid base64"
     )?;
-    let csr = X509Req::from_der(&certificate_bytes).map_err(|_| "Failed to parse the certificate")?;
+    let csr = X509Req::from_pem(&certificate_bytes).map_err(|_| "Failed to parse the certificate request")?;
     Ok(IntermediateEnrollDto { csr })
   }
 
