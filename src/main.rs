04b2419be73cb0b896614bfa736b20453f672232
ayman2ppp2 <79267200+ayman2ppp2@users.noreply.github.com>
2026-02-04 22:38:36 +0000

    creating a branch for development to stop rebuilding on every push

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,6 @@ async fn main() -> std::io::Result<()> {
         Err(e) => panic!("error in the reading of the crypto_config from env :{}", e),
     };
     let crypto_data = web::Data::new(crypto_config);
-    
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
